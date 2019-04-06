6213624eb5b799acdc464f015366d13aae7415ad
Enrique de Larrazabal <enrique.de.larrazabal@gmail.com>
2019-04-06 20:54:38 +0800

    edits

--- projects/breakdown_converter/script.js
+++ projects/breakdown_converter/script.js
@@ -18,16 +18,37 @@ $(document).ready(function() {
 		// Store Breakpoint
 		var BP = $(".breakpoint-input").val();
 
+		// Clear Table
+		$(".output tbody *").remove();
+
 		// Make sure that AOG can be divided by BP
 		if (AOG.length % BP !== 0) {
-			alert("Input does not match column count.");
-		} else {
-			// Loop every breakpoint
-			
-		}
 
-		// Show Output
-		$(".output-container").show();
+			alert("Input does not match column count.");
+
+		} else {
+
+			// Create <tr> for every AOG/BP
+			for (var rn = 1; rn <= (AOG.length/BP); rn++) {
+				$(".output tbody").append("<tr class='tr-" + rn + "'>");
+			}
+
+			// Divide AOG into BP chunks
+			CAOG = chunkArray(AOG, BP);
+			console.log(CAOG);
+
+			// Every CAOG[x] into corresponding <tr class="tr-[x+1]">
+			for (var x = 0; x <= CAOG.length; x++) {
+				currn = x + 1;
+				for (; ; i) {
+				}
+			}
+				//$(".tr-" + currn + " td");
+
+			// Show Output
+			$(".output-container").show();
+
+		}
 
 		// Stop Page Transfer
 		return false;
@@ -35,3 +56,17 @@ $(document).ready(function() {
 	});
 
 });
+
+function chunkArray(myArray, chunk_size){
+	var index = 0;
+	var arrayLength = myArray.length;
+	var tempArray = [];
+	
+	for (index = 0; index < arrayLength; index += chunk_size) {
+		myChunk = myArray.slice(index, index+chunk_size);
+		// Do something if you want with the group
+		tempArray.push(myChunk);
+	}
+
+	return tempArray;
+}
