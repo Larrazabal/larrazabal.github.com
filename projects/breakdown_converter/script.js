6b3c24b0be7a6be3345327433eb0de0059b4d3bd
Enrique de Larrazabal <enrique.de.larrazabal@gmail.com>
2019-04-06 20:02:18 +0800

    edits

--- projects/breakdown_converter/script.js
+++ projects/breakdown_converter/script.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
-	
+
+	$(".convert").click(function() {
+
+		// Store text from input
+		var OG = $(".input").val();
+
+		// Convert OG to array AOG
+		var AOG = OG.split("\n");
+
+		// Make sure 2 < breakpoint < 99
+		if ($(".breakpoint-input").val() < 2) {
+			$(".breakpoint-input").val("2")
+		} else if ($(".breakpoint-input").val() > 99) {
+			$(".breakpoint-input").val("99")
+		}
+
+		// Store Breakpoint
+		var BP = $(".breakpoint-input").val();
+
+		// Make sure that AOG can be divided by BP
+		if (AOG.length % BP !== 0) {
+			alert("Input does not match column count.");
+		} else {
+			// Loop every breakpoint
+			
+		}
+
+		// Show Output
+		$(".output-container").show();
+
+		// Stop Page Transfer
+		return false;
+
+	});
+
 });
