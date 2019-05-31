06d3b92817f844a64022193b6c8d2090cf4e14ae
Enrique de Larrazabal <enrique.de.larrazabal@gmail.com>
2019-05-31 08:21:00 +0800

    edits

--- projects/fb_conversion_translator/script.js
+++ projects/fb_conversion_translator/script.js
@@ -41,6 +41,7 @@ $(document).ready(function() {
 	// Conversions Load
 	addConversionColumn(9);
 	addSelections(".conv");
+	resetConversions();
 
 
 	// Show Values Load
@@ -90,56 +91,55 @@ $(document).ready(function() {
 	}
 	function resetConversions() {
 		$(".conv-1-1").val("imp");
-		$(".conv-1-2").val("imp");
-		
+		$(".conv-1-2").val("click");
+
 		$(".conv-2-1").val("click");
-		$(".conv-2-2").val("click");
+		$(".conv-2-2").val("lpv");
 
 		$(".conv-3-1").val("lpv");
-		$(".conv-3-2").val("lpv");
+		$(".conv-3-2").val("atc");
 
-		$(".conv-4-1").val("vc");
-		$(".conv-4-2").val("vc");
+		$(".conv-4-1").val("atc");
+		$(".conv-4-2").val("ic");
 
-		$(".conv-5-1").val("atc");
-		$(".conv-5-2").val("atc");
+		$(".conv-5-1").val("ic");
+		$(".conv-5-2").val("pur");
 
-		$(".conv-6-1").val("ic");
-		$(".conv-6-2").val("ic");
+		$(".conv-6-1").val("lpv");
+		$(".conv-6-2").val("pur");
 
-		$(".conv-7-1").val("pi");
-		$(".conv-7-2").val("pi");
+		$(".conv-7-1").val("atc");
+		$(".conv-7-2").val("pur");
 
-		$(".conv-8-1").val("pur");
+		$(".conv-8-1").val("pi");
 		$(".conv-8-2").val("pur");
 
-		$(".conv-9-1").val("cpm");
-		$(".conv-9-2").val("cpm");
-
-		$(".conv-10-1").val("purconv");
-		$(".conv-10-2").val("purconv");
-
-		$(".conv-11-1").val("spent");
-		$(".conv-12-2").val("spent");
-
-		$(".conv-12-1").val("roas");
-		$(".conv-12-2").val("roas");
+		$(".conv-9-1").val("lpv");
+		$(".conv-9-2").val("vc");
 	}
 	/* End Conversions Functions ============================ */
 
 
 	/* Show Values Functions ============================ */
-
+	function addSvalColumn(amt) {
+		var curAmt = $(".conversion-container").length;
+		for (var i = curAmt + 1; i <= amt + curAmt; i++) {
+			$(".conversion-options .inside").append(
+				"<div class='cont conversion-container'>" +
+					"<span>Conversion " + i + ": </span>" +
+					"<select class='conv conv-" + i + "-1' name='conv-" + i + "-1' id='conv-" + i + "-1'></select>" +
+					"<span> &gt; </span>" +
+					"<select class='conv conv-" + i + "-2' name='conv-" + i + "-2' id='conv-" + i + "-2'></select>" +
+				"</div>");
+		}
+	}
 	/* End Show Values Functions ============================ */
 
 
 	/* Metrics Options Options ========================== */
 	$("#metric-add").click(function() {
 		addMetricColumn(1);
-
-		for (var i = 0; i <= selections.length - 1; i++) {
-			$(".metric-options .inside .column-container:last-child .col").append("<option value='" + selection_values[i] + "'>" + selections[i] + "</option>");
-		}
+		addSelections(".metric-options .inside .column-container:last-child .col");
 
 		return false;
 	});
@@ -151,7 +151,7 @@ $(document).ready(function() {
 	$("#metric-defaults").click(function() {
 		$(".metric-options .inside").empty();
 		addMetricColumn(12);
-		addMetricSelections();
+		addSelections(".col");
 		resetMetrics();
 
 		return false;
@@ -161,14 +161,21 @@ $(document).ready(function() {
 
 	/* Converion Options Options ======================== */
 	$("#conversions-add").click(function() {
+		addConversionColumn(1);
+		addSelections(".conversion-options .inside .conversion-container:last-child .conv");
 
 		return false;
 	});
 	$("#conversions-remove").click(function() {
+		$(".conversion-options .inside .conversion-container:last-child").remove();
 
 		return false;
 	});
 	$("#conversions-defaults").click(function() {
+		$(".conversion-options .inside").empty();
+		addConversionColumn(9);
+		addSelections(".conv");
+		resetConversions();
 
 		return false;
 	});
